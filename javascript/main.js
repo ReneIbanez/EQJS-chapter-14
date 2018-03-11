e8c6561159896d66e7ca0054c297e98d52e32ee4
Rene Ibanez <renedibanez17@gmail.com>
2018-03-10 21:24:09 -0500

    still trying to put it toghther

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+document.createElement('table');
+document.createElement("th","td");
+
+    const MOUNTAINS = [
+      {name: "Kilimanjaro", height: 5895, place: "Tanzania"},
+      {name: "Everest", height: 8848, place: "Nepal"},
+      {name: "Mount Fuji", height: 3776, place: "Japan"},
+      {name: "Vaalserberg", height: 323, place: "Netherlands"},
+      {name: "Denali", height: 6168, place: "United States"},
+      {name: "Popocatepetl", height: 5465, place: "Mexico"},
+      {name: "Mont Blanc", height: 4808, place: "Italy/France"}
+    ];
+    document.getElementById('mountains');
+      for(var i=0; i < MOUNTAINS.length; i++){
+        document.write
+    };
